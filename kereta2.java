82965291345e8b18dc41158086d7b2e2cf86d46d
Fathinzhfraa <102747107+Fathinzhfraa@users.noreply.github.com>
2022-04-01 23:10:58 +0700

    Create kereta2.java
    main class

---
+++ kereta2.java
@@ -0,0 +1,26 @@
+package tugas;
+import java.util.Scanner;
+public class kereta2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("----------PEMESANAN TIKET KERETA API BANDUNG-ACEH-BALI-SURABAYA--------------");
+        System.out.print("Masukkan nama : ");
+        String nama = sc.next();
+        System.out.print("Masukkan tujuan : ");
+        String Tujuan = sc.next().toUpperCase();
+        kereta1 objek = new kereta1();
+        objek.tujuan();
+        switch (Tujuan) {
+            case "BANDUNG":
+                System.out.print("pilih tingkat :");
+                int tingkat = sc.nextInt();
+                if (tingkat == 1) {
+                    System.out.println("Anda memilih kelas bisnis");
+                    System.out.println(objek.dataHarga(0,0));
+
+                } else if (tingkat == 2) {
+                    System.out.println("Anda memilih kelas Ekonomi");
+                    System.out.println(objek.dataHarga(1, 0));
+                    ;
+                } else {
+                    System.out.println("Anda memilih kelas Eksekutif");
